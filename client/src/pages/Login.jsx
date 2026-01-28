5115509352a0cc407c72958c0894e30368651358
Prabin Acharya <nikhilacharya381@gmail.com>
2026-01-28 16:50:32 +0530

    Add show/hide password feature

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -97,7 +97,7 @@ export default function Login() {
                   onClick={() => setShowPassword(!showPassword)}
                   className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
                 >
-                  {showPassword ? "🙈" : "👁️"}
+                  {showPassword ? "🚫" : "👁️"}
                 </button>
               </div>
             </div>
