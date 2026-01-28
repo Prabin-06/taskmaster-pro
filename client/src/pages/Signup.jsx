3d457c9b990e71314ca4a19bac02a64ed2ccf367
Prabin Acharya <nikhilacharya381@gmail.com>
2026-01-28 16:51:28 +0530

    Add show/hide password feature

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -157,7 +157,7 @@ export default function Signup() {
                   }
                   className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
                 >
-                  {showConfirmPassword ? "🙈" : "👁️"}
+                  {showConfirmPassword ? "🚫" : "👁️"}
                 </button>
               </div>
             </div>
